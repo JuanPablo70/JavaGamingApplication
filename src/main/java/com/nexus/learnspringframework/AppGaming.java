84f7097df81ff4330205a051229ee80f5fa5a0f5
Juan Pablo <juanpasan2000@gmail.com>
2024-04-14 20:50:37 -0500

    Using Spring Framework to manage Beans

--- src/main/java/com/nexus/learnspringframework/AppGaming.java
+++ src/main/java/com/nexus/learnspringframework/AppGaming.java
@@ -1,18 +1,16 @@
 package com.nexus.learnspringframework;
 
-import com.nexus.learnspringframework.game.GameRunner;
-import com.nexus.learnspringframework.game.MarioGame;
-import com.nexus.learnspringframework.game.PacmanGame;
-import com.nexus.learnspringframework.game.SuperContraGame;
+import com.nexus.learnspringframework.game.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppGaming {
 
     public static void main(String[] args) {
-        //var game = new MarioGame();
-        //var game = new SuperContraGame();
-        var game = new PacmanGame();
-        var gameRunner = new GameRunner(game);
-        gameRunner.run();
+
+        var context = new AnnotationConfigApplicationContext(GamingConfiguration.class);
+
+        context.getBean(GameRunner.class).run();
+
     }
 
 }
