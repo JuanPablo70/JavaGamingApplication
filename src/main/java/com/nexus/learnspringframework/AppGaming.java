a7a2c52f1258414a56f485a9ae4e5040e88827fd
Juan Pablo <juanpasan2000@gmail.com>
2024-05-01 16:26:02 -0500

    Getting Spring Framework to create and manage Java Objects

--- src/main/java/com/nexus/learnspringframework/AppGaming.java
+++ src/main/java/com/nexus/learnspringframework/AppGaming.java
@@ -1,13 +1,15 @@
 package com.nexus.learnspringframework;
 
 import com.nexus.learnspringframework.game.*;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.*;
 
+@Configuration
+@ComponentScan("com.nexus.learnspringframework.game")
 public class AppGaming {
 
     public static void main(String[] args) {
 
-        var context = new AnnotationConfigApplicationContext(GamingConfiguration.class);
+        var context = new AnnotationConfigApplicationContext(AppGaming.class);
 
         context.getBean(GameRunner.class).run();
 
--- src/main/java/com/nexus/learnspringframework/game/SuperContraGame.java
+++ src/main/java/com/nexus/learnspringframework/game/SuperContraGame.java
@@ -1,5 +1,8 @@
 package com.nexus.learnspringframework.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SuperContraGame implements GamingConsole {
 
     public void up(){
